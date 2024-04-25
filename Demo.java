d4f74a2b4e68da29599d868108715163c71092fc
Naveen <bnaveen2255@gmail.com>
2024-04-25 12:50:23 +0530

    file updated

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ class Demo{
 public static void main(String arg[]){
 
 System.out.println("hello naveeen");
-int sum=sum(10,30); //Hello Mr.Bheemesh
+int sum=sum(10,30); //Hello Mr.Bheemesh Reels
 System.out.println(sum);
 
 }
